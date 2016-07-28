c36b7a3c79f480757d64e8a956d9e73d2aa14ff3
creativeArtie <creativeArtie@users.noreply.github.com>
2016-07-28 12:09:45 -0700

    Add files via upload

--- qunit/modelsNames.js
+++ qunit/modelsNames.js
@@ -6,17 +6,22 @@ QUnit.test( "Start Error", function( assert ) {
 });
 QUnit.test( "Text Error", function( assert ) {
   assert.throws( function(){
-    chainer.models("a_bc", function(){});
+    chainer.model("a_bc", function(){});
   }, "Namespace string pattern is wrong: a_bc" );
 });
 QUnit.test( "Repeat Error", function( assert ) {
   assert.throws( function(){
-    chainer.models("abc", function(){});
-    chainer.models("abc", function(){});
+    chainer.model("abc", function(){});
+    chainer.model("abc", function(){});
   }, "Model with this namespace is in use: abc" );
 });
 QUnit.test( "End Error", function( assert ) {
   assert.throws( function(){
-    chainer.models("_abc", function(){});
+    chainer.modes("_abc", function(){});
   }, "Namespace string pattern is wrong: abc." );
+});
+QUnit.test( "Basic" , function( assert ){
+  chainer.model("joy", function(){});
+  chainer.model("test.123", function(){});
+  assert.expect(0);
 });
